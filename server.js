8b2285ee518a559589dcd2fca192d8f54de4c042
Logesh-bruce <logeshmg432006@gmail.com>
2026-07-18 07:53:10 +0530

    Add get endpoint for habits

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const app = express();
 
+const habits = [
+    {
+        id: 1,
+        name: "Meditate",
+        goal: "10 minutes daily"
+    },
+    {
+        id: 2,
+        name: "Read",
+        goal: "20 minutes daily"
+    }
+];
+
+app.get("/api/habits", (req, res) => {
+    res.json(habits);
+});
+
 app.listen(3000, () => {
     console.log("HabitStack API is running on port 3000");
 });
